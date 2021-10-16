c12a8e0b181db02c798ee265e3493da164f12f28
Nishant kumar Gupta <nishantg1210@gmail.com>
2021-10-17 02:28:51 +0530

    feat: buttons done

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ var peer = new Peer(undefined,{
 let myVideoStream;
 navigator.mediaDevices.getUserMedia({
     video:true,
-    audio:false
+    audio:true
 }).then(stream =>{
     myVideoStream = stream;
     addVideoStream(myVideo,stream);
@@ -39,7 +39,7 @@ navigator.mediaDevices.getUserMedia({
 
     socket.on("createMessage", message => {
         $("ul").append(`<li class="message"><strong>user</strong><br/>${message}</li>`);
-        // scrollToBottom()
+        scrollToBottom()
     })
 })
 
@@ -64,3 +64,63 @@ const addVideoStream = (video, stream)=>{
     })
     videoGrid.append(video);
 }
+
+const scrollToBottom = () => {
+    let d = $('.main__chat_window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getAudioTracks()[0].enabled = false;
+      setUnmuteButton();
+    } else {
+      setMuteButton();
+      myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+  }
+
+  const setMuteButton = () => {
+    const html = `
+      <i class="fas fa-microphone"></i>
+      <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
+    
+  const setUnmuteButton = () => {
+    const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+      <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
+  
+  const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getVideoTracks()[0].enabled = false;
+      setPlayVideo()
+    } else {
+      setStopVideo()
+      myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+  }
+  
+  const setStopVideo = () => {
+    const html = `
+      <i class="fas fa-video"></i>
+      <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+  }
+  
+  const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+      <span>Play Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+  }
+  
