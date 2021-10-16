5ad2973a060cf222c8910eac6e7507d589d8493d
Nishant kumar Gupta <nishantg1210@gmail.com>
2021-10-17 02:33:55 +0530

    fix:port sorted

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ io.on('connection', socket => {
 
 
 
-server.listen(3000,()=>{
-    console.log("App up and listening at port 3000");
+server.listen(process.env.PORT||3000,()=>{
+    console.log("App up");
 });
