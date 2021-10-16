fbea2baad9ba54f4c1a8d80698c8b3c3a663e1fc
Nishant kumar Gupta <nishantg1210@gmail.com>
2021-10-17 00:21:24 +0530

    feat: buttons css done

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
-const express =require("express");
+const express =require('express');
+const app= express();
+
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server);
+const {ExpressPeerServer} = require('peer');
+const peerServer = ExpressPeerServer(server,{
+    debug:true
+});
 const {v4:uuidv4}= require('uuid');
 
-const app= express();
-const server = require('http').Server(app);
+app.use('/peerjs', peerServer);
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
-app.get('/',(req,res)=>{
+
+app.get('/', (req,res) => {
     res.redirect(`/${uuidv4()}`);
 })
 
-app.get('/:room',(req,res)=>{
-    res.render('room',{roomId:req.params.room})
+app.get('/:room', (req,res) => {
+    res.render('room', { roomId:req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId,userId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected',userId);
+    })
 })
 
 
 
 
-
-
-
-
-server.listen(3000);
+server.listen(3000,()=>{
+    console.log("App up and listening at port 3000");
+});
